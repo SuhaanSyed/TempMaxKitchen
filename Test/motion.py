14064920a4df59dedeb6353ad2023b9e34fc620b
SuhaanSyed <67168522+SuhaanSyed@users.noreply.github.com>
2020-10-17 14:56:41 -0500

    Create motion.py

---
+++ Test/motion.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+sensorPin = 16 # the gpio pin number
+
+#GPIO setup
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(sensorPin, GPIO.IN, GPIO.PUD_DOWN) # sets the pin number as input
+
+if GPIO.input(sensorPin) == 1: # if motion is detected
+  print("Initiate the camera")
+
+else: # if not detected
+  print("Don't do anything that")
